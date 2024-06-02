21b492375918a811765ea98e27a110e8173c1f2d
Kiazim70 <96169057+Kiazim70@users.noreply.github.com>
2024-06-02 11:54:27 +0200

    commit some class for updating project

--- src/main/java/com/angularspringbootecommerce/backend/repository/BillRepository.java
+++ src/main/java/com/angularspringbootecommerce/backend/repository/BillRepository.java
@@ -1,10 +1,6 @@
 package com.angularspringbootecommerce.backend.repository;
 
 import com.angularspringbootecommerce.backend.models.Bill;
-import com.angularspringbootecommerce.backend.models.Product;
-import com.google.common.primitives.Longs;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
